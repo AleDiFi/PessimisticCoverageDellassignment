53faadf7895dc0a1a03f2d95dad9d70df5ef8f3b
AleDiFi <alessandrodifilippo98@gmail.com>
2022-02-16 18:47:19 +0100

    code smell refactor

--- src/logic/java/com/example/pessimisticcoveragedellassignment/TriangleArea.java
+++ src/logic/java/com/example/pessimisticcoveragedellassignment/TriangleArea.java
@@ -2,7 +2,7 @@ package com.example.pessimisticcoveragedellassignment;
 
 public class TriangleArea {
 
-    public Double AreaCalculator(Double base, Double altezza) {
+    public Double areaCalculator(Double base, Double altezza) {
         Double result;
         result = (base * altezza) / 2;
         return result;
--- src/test/java/testareatriangolo/TestTriangleArea.java
+++ src/test/java/testareatriangolo/TestTriangleArea.java
@@ -1,3 +1,6 @@
+package testareatriangolo;
+
+
 import com.example.pessimisticcoveragedellassignment.TriangleArea;
 import org.junit.jupiter.api.Test;
 
@@ -6,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestTriangleArea {
 
     @Test
-    public void TestAreaCalculator(){
-        TriangleArea TestArea = new TriangleArea();
-        double output = TestArea.AreaCalculator(0.0,0.0);
+    public void testAreaCalculator(){
+        TriangleArea testArea = new TriangleArea();
+        double output = testArea.areaCalculator(0.0,0.0);
         assertEquals(Double.NaN, output);
     }
 }
